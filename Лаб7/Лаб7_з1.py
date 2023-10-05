7215167c25d96aea66a985c40bac1023d450add7
Manikalopolina <manikalopolina12@gmail.com>
2023-10-05 10:45:02 +0300

    задание 1

--- Лаб7/Лаб7_з1.py
+++
@@ -1,6 +0,0 @@
-languages = ["Python", "Java", "C", "C++", "JavaScript", "Pascal.ABC.NET", "Go"]
-all_tu = tuple(languages)
-new_tu = all_tu[:3]
-new_tu2 = all_tu[3:]
-print(new_tu)
-print(new_tu2)
---
+++ Лаб7/з1.py
@@ -0,0 +1,7 @@
+languages = ["Python", "Java", "C", "C++", "JavaScript", "Pascal.ABC.NET", "Go"]
+all_tu = tuple(languages)# Создаём кортеж из списка "languages"
+new_tu = all_tu[:3]# Создаём новый кортеж "new_tu" с первыми тремя элементами из "all_tu"
+new_tu2 = all_tu[3:]# Создаём новый котеж "new_tu2" с оставшимися элеметами из "all_tu"
+print(all_tu)# Выводим кортеж all_tu
+print(new_tu)# Выводим кортеж new_tu
+print(new_tu2)# Выводим кортеж new_tu2
