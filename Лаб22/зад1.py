3b7b81aaef747bad51c2ea7527fde40e5c341c93
Manikalopolina <manikalopolina12@gmail.com>
2024-02-09 14:54:25 +0300

    Лаб22

--- Лаб22/зад1.py
+++ Лаб22/зад1.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-result = requests.get("https://swapi.dev/api/people/3/") # получение кода страницы методом get
+result = requests.get("https://swapi.dev/api/people/3/")
 if result.status_code == 200:
-    json_dict = json.loads(result.text)  # десериализация JSON
+    json_dict = json.loads(result.text)
     json_dict['name'] = input("Введите имя: ")
     with open("swap.json", "w") as file:
-        json_text = json.dump(json_dict, file, indent=4)  # сериализация JSON
+        json_text = json.dump(json_dict, file, indent=4)
     # Доп:
     result_homeworld = requests.get(json_dict['homeworld'])
     print(result_homeworld.text)
